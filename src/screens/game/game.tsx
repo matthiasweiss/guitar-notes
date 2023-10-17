6a2db1394be6d3d65b076f824015fc167bea2ab5
Matthias Weiß <mail@matthiasweiss.at>
2023-10-17 19:28:21 +0200

    Make game work

--- src/screens/game/game.tsx
+++ src/screens/game/game.tsx
@@ -1,44 +1,75 @@
-import { ReactNode, useState } from 'react';
+import { ReactNode, useMemo, useState } from 'react';
 import { GuitarString, useGuitarStrings } from '../../hooks/use-guitar-strings';
 import { Note, useNotes } from '../../hooks/use-notes';
 import { calculateFret } from './calculate-fret';
 
 type State = 'initial' | 'running' | 'stopped';
 
-export const Game = () => {
-  const [state, setState] = useState<State>('initial');
-  const [selectedFret, setSelectedFret] = useState<number | null>(null);
-  const [areFretsHighlighted, setAreFretsHighlighted] = useState(false);
+type Question = {
+  note: Note;
+  guitarString: GuitarString;
+  fret: number;
+};
 
+type Answer = Question & {
+  selectedFret: number;
+};
+
+export const Game = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answers, setAnswers] = useState<Answer[]>([]);
+  const correctAnswers = answers.filter(
+    ({ fret, selectedFret }) => fret === selectedFret,
+  );
+
+  const [state, setState] = useState<State>('initial');
   const { randomNote, notes } = useNotes();
   const { randomGuitarString } = useGuitarStrings();
 
-  const currentNote: Note = randomNote();
-  const currentString: GuitarString = randomGuitarString();
-  const currentFret = calculateFret({
-    note: currentNote,
-    guitarString: currentString,
-  });
+  const current = useMemo(() => {
+    const note = randomNote();
+    const guitarString = randomGuitarString();
+    const fret = calculateFret({ note, guitarString });
+
+    return {
+      note,
+      guitarString,
+      fret,
+    };
+  }, [currentQuestion]);
+
+  const [selectedFret, setSelectedFret] = useState<number | null>(null);
+  const [areFretsHighlighted, setAreFretsHighlighted] = useState(false);
 
   const start = () => {
     setState('running');
   };
 
+  const clearAnswer = () => {
+    setAreFretsHighlighted(false);
+    setSelectedFret(null);
+  };
+
+  const showNextQuestion = () => {
+    clearAnswer();
+    setCurrentQuestion((_currentQuestion) => _currentQuestion + 1);
+  };
+
   const stop = () => {
+    clearAnswer();
+    setAnswers([]);
     setState('stopped');
   };
 
   const restart = () => {
-    setState('initial');
+    setState('running');
   };
 
   const checkAnswer = (selectedFret: number) => {
     setSelectedFret(selectedFret);
     setAreFretsHighlighted(true);
 
-    console.log(`Answer: ${selectedFret}, Correct: ${currentFret}`);
-    if (selectedFret === currentFret) {
-    }
+    setAnswers((_answers) => [..._answers, { ...current, selectedFret }]);
   };
 
   const calculateClasses = (fret: number) => {
@@ -46,7 +77,7 @@ export const Game = () => {
       return '';
     }
 
-    if (fret === currentFret) {
+    if (fret === current.fret) {
       return 'bg-green-500';
     }
 
@@ -57,13 +88,14 @@ export const Game = () => {
 
   const componentMap: Record<State, ReactNode> = {
     initial: (
-      <>
+      <div>
         Initial <button onClick={start}>Start</button>
-      </>
+      </div>
     ),
     running: (
       <div className="flex flex-col">
-        Which fret is the {currentNote} note on the {currentString} string?
+        Which fret is the {current.note} note on the {current.guitarString}{' '}
+        string?
         {notes.map((note, index) => {
           return (
             <button
@@ -75,13 +107,17 @@ export const Game = () => {
             </button>
           );
         })}
+        <button onClick={showNextQuestion}>Next</button>
         <button onClick={stop}>Stop</button>
+        <div>
+          {correctAnswers.length}/{answers.length} correct answers so far
+        </div>
       </div>
     ),
     stopped: (
-      <>
+      <div>
         Stopped <button onClick={restart}>Restart</button>
-      </>
+      </div>
     ),
   };
 
