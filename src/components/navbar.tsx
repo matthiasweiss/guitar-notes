868f619652d3bf88ccf39ecf3d6042deaa5f81e1
Matthias Weiß <mail@matthiasweiss.at>
2023-11-18 12:32:56 +0100

    Tweak layout

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,8 +6,8 @@ export const Navbar = () => {
   const { pathname } = useLocation();
 
   return (
-    <div className="flex h-full flex-col gap-4 rounded-md bg-slate-50 px-6 py-10 md:px-8 lg:h-fit lg:p-10 lg:pb-44 lg:pr-20">
-      <ul className="flex flex-col gap-4">
+    <div className="flex h-full flex-col items-center gap-4 rounded-md bg-slate-50 px-6 py-10 md:h-fit md:w-full md:px-0 md:py-4">
+      <ul className="flex w-full flex-col gap-4 md:max-w-3xl md:flex-row md:gap-8 md:px-4">
         {Object.entries(ROUTES).map(([name, { path }]) => {
           return (
             <li key={path}>
--- src/screens/layout.tsx
+++ src/screens/layout.tsx
@@ -3,12 +3,10 @@ import { Navbar } from '../components/navbar';
 
 export const Layout = () => {
   return (
-    <div className="flex h-screen w-screen justify-center">
-      <div className="flex h-screen w-full max-w-5xl lg:max-w-3xl lg:py-10">
-        <Navbar />
-        <div className="flex flex-1 justify-center p-4">
-          <Outlet />
-        </div>
+    <div className="flex h-screen w-screen md:flex-col md:items-center">
+      <Navbar />
+      <div className="flex w-full max-w-3xl flex-1 justify-center p-4">
+        <Outlet />
       </div>
     </div>
   );
